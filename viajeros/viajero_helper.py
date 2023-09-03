3cfea9ec3e68ba585c9e30619a0b4a7812b45205
serranoycandela <serranoycandela@gmail.com>
2023-09-03 13:53:27 -0600

    viajero helper refinado

--- viajeros/viajero_helper.py
+++ viajeros/viajero_helper.py
@@ -1,24 +1,28 @@
 #-*- encoding: utf-8 -*-
-import xml.etree.ElementTree as etree
+#import xml.etree.ElementTree as etree
 import os
-import xlsxwriter
+#import xlsxwriter
 import sys
 from PySide2.QtCore import Qt
-from PySide2 import QtGui, QtCore, QtWidgets
-from PySide2.QtWidgets import QApplication, QMainWindow, QInputDialog, QFileDialog, QWidget, QTableWidget,QTableWidgetItem,QPushButton,QListView,QAbstractItemView,QTreeView,QMessageBox
-from PySide2.QtCore import QFile, QRect
+#from PySide2 import QtGui, QtCore, QtWidgets
+from PySide2.QtWidgets import QApplication, QMainWindow, QInputDialog, QFileDialog, QTableWidgetItem, QListView, QMessageBox, QLineEdit
+#from PySide2.QtCore import QFile, QRect
 from PySide2.QtGui import QIcon
 from gui import Ui_MainWindow
 import json
 from os.path import dirname, realpath, join, abspath
-from openpyxl.styles import Alignment
+#from openpyxl.styles import Alignment
 
-from openpyxl import load_workbook,  Workbook
-from openpyxl.utils import get_column_letter
-from openpyxl.styles import PatternFill, Font
+from openpyxl import load_workbook#,  Workbook
+#from openpyxl.utils import get_column_letter
+#from openpyxl.styles import PatternFill, Font
 from openpyxl.worksheet.datavalidation import DataValidation
 from FacturasLocal import FacturaLocal as Factura
 import pandas as pd
+from datetime import datetime
+
+# pyinstaller.exe --noconsole .\viajero_helper.py
+# pyside2-uic gui.ui -o gui.py
 
 if getattr(sys, 'frozen', False):
     # we are running in a bundle
@@ -87,10 +91,8 @@ class MainWindow(QMainWindow):
             trip_folder = file_dialog.selectedFiles()[0]
             self.prellenar_formato(trip_folder)
         
-
-
-    def prellenar_formato(self, trip_folder):
-
+    def get_metadata(self):
+        
         if os.path.isfile(os.path.join(appDataDirectory,"proyecto.txt")):
             with open(os.path.join(appDataDirectory,"proyecto.txt"), 'r') as f:
                 proyecto_guardado = f.readline()
@@ -101,7 +103,7 @@ class MainWindow(QMainWindow):
             with open(os.path.join(appDataDirectory,"viajero.txt"), 'r') as f:
                 viajero_guardado = f.readline()
         else:
-            viajero_guardado = False
+            viajero_guardado = "Ingresa tu RFC"
         
         proyectos_sheet_id = "1a3-O0OvzDQect8EszxeeDrKbrLxNaw2Lti7t37Usfq8"
         r = "https://docs.google.com/spreadsheets/export?id={}&exportFormat=csv".format(proyectos_sheet_id)
@@ -112,7 +114,7 @@ class MainWindow(QMainWindow):
         else:
             id_proyecto = 0
         
-        proyecto, ok = QInputDialog().getItem(self, "QInputDialog().getItem()",
+        proyecto, ok = QInputDialog().getItem(self, "Proyecto",
                                             "Proyecto:", lista_proyectos, id_proyecto, False)
         if ok and proyecto:
             with open(os.path.join(appDataDirectory,"proyecto.txt"), "w") as f:
@@ -121,19 +123,41 @@ class MainWindow(QMainWindow):
         viajeros_sheet_id = "14_DmyXmBJx7eof65VelFrkrGKK4Rw6ZSR0nRSFFiio4"
         r = "https://docs.google.com/spreadsheets/export?id={}&exportFormat=csv".format(viajeros_sheet_id)
         df_viajeros = pd.read_csv(r)
-        lista_viajeros = df_viajeros['NOMBRE'].tolist()
-        if viajero_guardado:
-            id_viajero = lista_viajeros.index(viajero_guardado)
-        else:
-            id_viajero = 0
+        
+        viajero_rfc, ok = QInputDialog().getText(self, "RFC",
+                                     "RFC:", QLineEdit.Normal,
+                                     viajero_guardado)
+        if ok and viajero_rfc:
+            viejero_row = df_viajeros[df_viajeros['RFC'] == viajero_rfc] 
+            viajero = viejero_row['NOMBRE'].loc[viejero_row.index[0]]
+            coordinador = viejero_row['COORDINADOR'].loc[viejero_row.index[0]]
+            empresa = viejero_row['EMPRESA'].loc[viejero_row.index[0]]
+            with open(os.path.join(appDataDirectory,"viajero.txt"), "w") as f:
+                f.write(viajero_rfc)
 
-        viajero, ok = QInputDialog().getItem(self, "QInputDialog().getItem()",
-                                            "Proyecto:", lista_viajeros, id_viajero, False)
-        if ok and viajero:
-             with open(os.path.join(appDataDirectory,"viajero.txt"), "w") as f:
-                f.write(viajero)
+        # viajero, ok = QInputDialog().getItem(self, "Nombre",
+        #                                     "Nombre:", lista_viajeros, id_viajero, False)
+        # if ok and viajero:
+            # with open(os.path.join(appDataDirectory,"viajero.txt"), "w") as f:
+            #     f.write(viajero)
+
+
+        return [viajero, coordinador, proyecto, empresa]
         
 
+
+    def prellenar_formato(self, trip_folder):
+
+        try:
+            
+            metadatos = self.get_metadata()
+            viajero = metadatos[0]
+            coordinador = metadatos[1]
+            proyecto = metadatos[2]
+            empresa = metadatos[3]
+        except:
+            QMessageBox.information(self, "Information", "Problema de conexión" )
+            return 0
 
         self.ui.textBrowser.clear()
         self.ui.tableWidget.clear()
@@ -142,23 +166,15 @@ class MainWindow(QMainWindow):
         self.ui.tableWidget.setRowCount(6)
         self.ui.tableWidget.repaint()
 
-        empleado_folder = os.path.split(trip_folder)[1]
-
-        viaje_folder = os.path.split(os.path.split(trip_folder)[0])[1]
-
-        home = os.path.expanduser('~')
-        template_folder = os.path.join(home, 'Documents', 'huiini')
-        wb_template = load_workbook(os.path.join(template_folder,"FORMATO_template.xlsx"))
+        wb_template = load_workbook(os.path.join(appDataDirectory,"FORMATO_template_"+empresa+".xlsx"))
         ws_template = wb_template[wb_template.get_sheet_names()[0]]
-
-
-        self.xlsx_path = os.path.join(trip_folder,"FORMATO_"+empleado_folder.replace(" ","-")+"_"+viaje_folder.replace(" ","-")+".xlsx")
-
 
         row_f = 11
         #folder = os.path.split(path)[1]
         por_tipo={"Total":0,"Transporte":0,"Alimentos":0,"Hospedaje":0,"Otros":0}
         hay_facturas = False
+        date_format = '%Y-%m-%d'
+        fechas = []
         for archivo in os.listdir(trip_folder):
             if archivo.endswith(".xml"):
                 row_f += 1
@@ -168,6 +184,7 @@ class MainWindow(QMainWindow):
                 iva = factura.traslados["IVA"]["importe"]
                 uuid = factura.UUID
                 fecha = factura.fechaTimbrado.split("T")[0]
+                fechas.append(datetime.strptime(fecha, date_format))
                 provedor = factura.EmisorNombre
                 clave_ps = factura.conceptos[0]['clave_concepto']
                 tipo = self.tipo_de_gasto(clave_ps)
@@ -182,28 +199,40 @@ class MainWindow(QMainWindow):
                 por_tipo["Total"]+=total
                 hay_facturas = True
 
-
+        ws_template.cell(8, 1, viajero)#nombre
+        ws_template.cell(8, 3, proyecto)#proyecto
+        ws_template.cell(57, 4, viajero)#firma
+        ws_template.cell(57, 1, coordinador)#coordinador
+        
         if hay_facturas:
-            self.ui.textBrowser.append("Procesando: "+empleado_folder+" "+viaje_folder)
-            self.ui.tableWidget.setItem(1,0,self.esteItem(empleado_folder,""))
+            fechas.sort(reverse=False)
+            fecha_inicio = fechas[0]
+            fecha_fin = fechas[len(fechas)-1]
+            if fecha_inicio == fecha_fin:
+                fechas_str = fecha_fin.strftime('%d-%m-%Y')
+            else:
+                fechas_str = fecha_inicio.strftime('%d-%m-%Y') + "_" + fecha_fin.strftime('%d-%m-%Y')
+            
+            self.xlsx_path = os.path.join(trip_folder,"FORMATO_"+viajero.replace(" ","-")+"_"+fechas_str+".xlsx")
+            self.ui.textBrowser.append("Procesando: "+trip_folder)
+            self.ui.tableWidget.setItem(1,0,self.esteItem(fechas_str,""))
             self.ui.tableWidget.setItem(1,1,self.esteItem(str(por_tipo["Total"]),""))
             self.ui.tableWidget.setItem(1,2,self.esteItem(str(por_tipo["Transporte"]),""))
             self.ui.tableWidget.setItem(1,3,self.esteItem(str(por_tipo["Hospedaje"]),""))
             self.ui.tableWidget.setItem(1,4,self.esteItem(str(por_tipo["Alimentos"]),""))
             self.ui.tableWidget.setItem(1,5,self.esteItem(str(por_tipo["Otros"]),""))
-            try:
-                mes = viaje_folder.split(" ")[1].title()
-                dia = viaje_folder.split(" ")[0]
-            except:
-                mes = ""
-                dia = ""
-            ws_template.cell(5, 7, mes)#dia
-            ws_template.cell(5, 5, dia)#mes
+            
+            ws_template.cell(4, 6, fecha_inicio.strftime('%d-%m-%Y'))#fecha_inicio
+            ws_template.cell(5, 6, fecha_fin.strftime('%d-%m-%Y'))#fecha_fin
             self.ui.textBrowser.append("Formato de gastos de viaje creado en: "+self.xlsx_path)
             wb_template.save(self.xlsx_path)
             self.ui.excel_button.setEnabled(True)
         else:
             QMessageBox.information(self, "Information", "No hay facturas en este folder" )
+
+        if len(sys.argv) > 1:
+            self.abre_excel()
+            sys.exit()
 
     def ponEncabezado(self):
         self.ui.tableWidget.setHorizontalHeaderItem (0, QTableWidgetItem("Viaje"))
@@ -218,9 +247,11 @@ class MainWindow(QMainWindow):
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
-    window.show()
-    app.setWindowIcon(QIcon(join(scriptDirectory,'logo.ico')))
-    window.setWindowIcon(QIcon(join(scriptDirectory,'logo.ico')))
     if len(sys.argv) > 1:
         window.prellenar_formato(sys.argv[1])
+        
+    else:
+        window.show()
+        app.setWindowIcon(QIcon(join(scriptDirectory,'logo.ico')))
+        window.setWindowIcon(QIcon(join(scriptDirectory,'logo.ico')))
     sys.exit(app.exec_())
